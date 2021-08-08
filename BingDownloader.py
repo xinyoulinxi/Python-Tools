607e8292dcdfd83b6af4ca15f73498d2fcd15ebc
xinyoulinxi <1135479396@qq.com>
2021-08-08 23:33:45 +0800

    impl bing wallpaper downloader

--- BingDownloader.py
+++ BingDownloader.py
@@ -1,20 +1,10 @@
 
 import requests
 import random
-import wget
-
-def GetARandomJpgName():
-    rand_int = random.randint(1,10000)
-    rand_char = random.choice("NotToday")
-    return str(rand_char)+str(rand_int)+".jpg"
-
-def IsImageResponse(content):
-    http_response = ["head","body"]
-    for html_content in http_response :
-        html_content = bytes(html_content, encoding = "utf8")
-        if content.find(html_content) ==-1:
-            return True
-    return False
+import json
+import os
+import time
+from datetime import datetime, timedelta
 
 def GetRequestContent(address,payload=""):
     headers = requests.utils.default_headers()
@@ -22,36 +12,62 @@ def GetRequestContent(address,payload=""):
     {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
     })
-    r = requests.get(address,headers=headers)
-    print(address,payload,r.content.decode())
+    r = requests.get(address,headers=headers,params=payload)
     return r.content
 
 def DownloadPic(address,name):
-    content = wget.dow
+    content = GetRequestContent(address)
     with open(name, "wb") as f:
         f.write(content)
+        print("image download successful, saved in: "+name)
         return True
-# api ref： https://github.com/TimothyYe/bing-wallpaper
-# https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=zh-CN ,获取当天的bing壁纸
 
-# https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=zh-CN
-today_pic_api = "https://bing.biturl.top/?resolution=1920&index=0&mkt=zh-CN"
-bing_pic_api = "https://bing.biturl.top"
-def GetBingBgUrl(index):
-    payload = {'resolution': '1920', 'format': 'json','index':str(index),'mkt':'zh-CN'}
-    content = GetRequestContent(today_pic_api)
-    # ret = os.system("wget '"+today_pic_api+"'")
-    print(content.decode())
+# api ref： https://stackoverflow.com/questions/10639914/is-there-a-way-to-get-bings-photo-of-the-day
+# https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1 ,获取当天的bing壁纸
+
+# bing page
+base_url = "https://www.bing.com"
+# bing pic get base url
+base_pic_json_api = "https://www.bing.com/HPImageArchive.aspx"
+# bing pic json api
+today_pic_api = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+
+def GetBingBgUrl(params=[]):
+    content = GetRequestContent(base_pic_json_api,payload=params)
     if len(content)!=0:
-        return content["url"]
+        decoded_contene = content.decode()
+        json_data = json.loads(decoded_contene)
+        url = base_url+json_data["images"][0]["url"]
+        print("image url = ",url)
+        return url
     return ""
-import os
-import time
+
+mkt_list = ["zh-CN","en-US", "ja-JP", "en-AU", "en-UK", "de-DE", "en-NZ", "en-CA"]
+
+# index is the bing image index from today , 1 is tomorrow, 2 for today-1 ...
+def DownLoadBingImg(path,index=0):
+    bing_pic_date = datetime.today() - timedelta(days = index )
+    bing_pic_date = bing_pic_date.strftime("%Y_%m_%d")
+    print(bing_pic_date)
+    # all mkt is same pic
+    # just get china 
+    payload = {'format': 'js', 'idx': index,'n':1,'mkt':mkt_list[0]}
+    bing_img_url = GetBingBgUrl(payload)
+    # get bing image
+    DownloadPic(bing_img_url,path+bing_pic_date+".jpg")
+    
+# download all 8 day bing wallpaper
+
+def DownloadAllDayBingImg(path):
+    count = 7
+    while count >=0:
+        DownLoadBingImg(path,count)
+        count = count - 1
+
 def main():
     print(os.listdir("./"))
     if not "bing_bg" in os.listdir("./"):
         os.makedirs("./bing_bg/")
-    print(GetBingBgUrl('1'))
-
+    DownloadAllDayBingImg("./bing_bg")
 if __name__ == "__main__":
     main()
